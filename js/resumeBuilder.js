0ea458ff40596497b691d15e76dd742893af73ae
Yuan Jiang <yuanjiang@Yuans-MacBook-Pro.local>
2016-01-31 16:56:46 -0500

    work section works

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,7 +1,7 @@
 var bio = {
     "name": "Yuan Jiang",
     "role": "Developer",
-    "contact" : {
+    "contacts" : {
       "mobile": "+1(603)667-8324",
       "email": "yuan.jiang.gr@dartmouth.edu",
       "github": "jiangyuanhk",
@@ -31,19 +31,20 @@ var education = {
     {
       "title": "JavaScript Basics",
       "school": "Udacity",
-      "date": 2014,
+      "dates attended": "Jan 2016",
       "url": "https://goo.gl/EBAkI1"
     }
   ]
 };
-
 
 var work = {
   "jobs": [
     {
       "employer": "Facebook",
       "title": "Software Engineer",
-      "dates": "June 2016 - "
+      "dates": "June 2016 - ",
+      "description": "Starting off ~~",
+      "location": "Menlo Park, CA"
     }
   ]
 }
@@ -52,11 +53,34 @@ var projects = {
   "projects": [
     {
       "title": "Sample Project 1",
-      "dates": "2014",
-      "description": "Sample Project 1 description"
+      "dates worked": "2014",
+      "description": "Sample Project 1 description",
+      "images": ["http://placebear.com/200/200"]
     }
   ]
 }
+
+var displayWork = function displayWork () {
+  for (job in work.jobs) {
+    $("#workExperience").append(HTMLworkStart);
+
+    var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+    var formmatedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+    $(".work-entry:last").append(formattedEmployer + formmatedTitle);
+    
+    var formmatedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+    $(".work-entry:last").append(formmatedDates);
+
+    var formmatedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+    var formmatedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+    $(".work-entry:last").append(formmatedDescription);
+    $(".work-entry:last").append(formmatedLocation);
+  }
+}
+
+
+
+
 
 
 
